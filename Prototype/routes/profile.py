417a075cfd31d762920cd4906d52f5f138f1ad33
Vmrc <vmrcgh@protonmail.com>
2021-03-21 18:59:33 +0100

    Added dashboard in profile

--- Prototype/routes/profile.py
+++ Prototype/routes/profile.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
+from database.database import db
+from models.Post import Post
+import os
 
 profile = Blueprint('profile', __name__, template_folder='templates')
 
@@ -7,5 +10,26 @@ profile = Blueprint('profile', __name__, template_folder='templates')
 @profile.route('/')
 @login_required
 def profile_index():
+    app_path = os.path.dirname(os.path.abspath(__name__))
+    images_path = os.path.join(app_path, 'static/images_storage')
+
     posts = current_user.posts
-    return render_template('profile/profile.html.jinja2', posts=posts)
+    user_id = current_user.get_id()
+    posts_user = db.session.query(Post).filter(Post.user_id == user_id)
+    posts_global = db.session.query(Post)
+
+    number_images_user = posts_user.count()
+    number_images_global = posts_global.count()
+    images_global_size, images_user_size = 0, 0
+
+    for post in posts_global:
+        images_global_size += os.path.getsize(os.path.join(images_path, post.image_name))
+    for post in posts_user:
+        images_user_size += os.path.getsize(os.path.join(images_path, post.image_name))
+
+    images_global_size = str(round(images_global_size / (1024 * 1024), 3))  # Convert to megabytes
+    images_user_size = str(round(images_user_size / (1024 * 1024), 3))  # Convert to megabytes
+
+    return render_template('profile/profile.html.jinja2', posts=posts, number_images_user=number_images_user,
+                           number_images_global=number_images_global, images_global_size=images_global_size,
+                           images_user_size=images_user_size)
